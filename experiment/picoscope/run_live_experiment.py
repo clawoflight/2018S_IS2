c02300b2fbd7a5ad3ecd02284132e8c79fcb4f0e
Bennett Piater <bennett@piater.name>
2018-05-10 13:22:41 +0200

    comment and rename script

--- experiment/picoscope/run_live_experiment.py
+++ experiment/picoscope/run_live_experiment.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 from picoscope import ps3000a
 
 import matplotlib.pyplot as plt
@@ -13,6 +14,10 @@ import time
 numSamples = 2000000
 
 def setupScope():
+    """Open a connection to the picoscope and setup channel & sampling rate.
+
+    :return: [picoscope, sampling rate]
+    """
     ps = ps3000a.PS3000a()
 
     res = ps.setSamplingFrequency(2E6, numSamples)
@@ -21,32 +26,60 @@ def setupScope():
     return [ps, res[0]]
 
 def plot(data, freq, bstart, bend):
+    """Plot the given data.
+
+    :param data: the raw voltage values
+    :param freq: used sampling frequency
+    :param bstart: starting brightness level
+    :param bend: end brightness level
+    """
+    # Perform STFT (https://en.wikipedia.org/wiki/Short-time_Fourier_transform)
     f, t, Zxx = scipy.signal.stft(data, freq, nperseg=1000)
 
-    plt.rcParams['agg.path.chunksize'] = 10000
+    plt.rcParams['agg.path.chunksize'] = 10000 # allows opening huge graphs
+
+    # Plot frequencies over time
     plt.pcolormesh(t, f, np.abs(Zxx))
+    # Add a horizontal line to make it easier to see differences
     plt.axhline(y=700000, color="yellow")
+
+    # Label the graph
     plt.ylabel('Frequency [Hz]')
     plt.xlabel('Time [sec]')
     plt.title("Brightness change from {} to {}".format(bstart, bend))
     plt.tight_layout()
+    # Show the graph
     plt.show()
 
 def main():
+    # Parse command-line arguments
     bstart = int(sys.argv[1])
     bend = int(sys.argv[2])
+
+    # Set initial hue brightness
     os.system("hueadm light 1 \"={}\"".format(bstart))
+
+    # Setup the osci
     [scope, rate] = setupScope()
+
+    # Start collecting data from the osci
     scope.runBlock()
+
+    # Change brightness levels
     os.system("hueadm light 1 \"={}\"".format(bend))
     time.sleep(.1)
     os.system("hueadm light 1 \"={}\"".format(bstart))
 
+    # Wait until data was collected
     while not scope.isReady():
         pass
 
+    # Download data from the osci
     data = scope.getDataV(0, numSamples)
+
     plot(data, rate, bstart, bend)
+
+    # Serialize the data for later re-use
     with open("{}-{}.pickle".format(bstart, bend), 'wb') as fp:
         pickle.dump(data, fp)
 
